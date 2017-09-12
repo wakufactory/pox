fc50fc3b70f0aaba23009378c5df9691d5f9aa0b
wakufactory <waku@lang.co.jp>
2017-09-12 17:36:37 +0900

    add group support

--- js/poxplayer.js
+++ js/poxplayer.js
@@ -78,6 +78,27 @@ PoxPlayer.prototype.set = function(d,param={}) {
 	this.active = true ;	
 	var POX = {src:d,can:this.can,wwg:wwg,synth:this.synth,param:param} ;
 	this.pox = POX ;
+	function V3add() {
+		var x=0,y=0,z=0 ;
+		for(var i=0;i<arguments.length;i++) {
+			x += arguments[i][0] ;y += arguments[i][1] ;z += arguments[i][2] ;
+		}
+		return [x,y,z] ;
+	}
+	function V3len(v) {
+		return Math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]) ;
+	}
+	function V3norm(v,s) {
+		var l = V3len(v) ;
+		if(s===undefined) s = 1 ;
+		return (l==0)?[0,0,0]:[v[0]*s/l,v[1]*s/l,v[2]*s/l] ;
+	}
+	function V3mult(v,s) {
+		return [v[0]*s,v[1]*s,v[2]*s] ;
+	}
+	function V3dot(v1,v2) {
+		return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2] ;
+	}
 
 	POX.setScene = (scene)=> {
 		this.setScene(scene) ;
@@ -238,6 +259,16 @@ console.log(r) ;
 			var d = render.getModelData(i) ;
 			var m = new CanvasMatrix4(d.bm) ;
 			if(d.mm) m.multRight(d.mm) ;
+			if(render.data.group) {
+				var g = render.data.group ;
+				for(var gi = 0 ;gi < g.length;gi++) {
+					var gg = g[gi] ;
+					if(!gg.bm) continue ;
+					for(var ggi=0;ggi<gg.model.length;ggi++) {
+						if(render.getModelIdx(gg.model[ggi])==i) m.multRight(gg.bm) ;
+					}
+				}
+			}
 			mod[i] = {
 				vs_uni:{
 					modelMatrix:new CanvasMatrix4(m).getAsWebGLFloatArray(),
