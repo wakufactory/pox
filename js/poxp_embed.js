374f3a39890d31119066fa724379739c04f42782
wakufactory <waku@lang.co.jp>
2019-03-26 19:21:57 +0900

    update embed sample

--- js/poxp_embed.js
+++ js/poxp_embed.js
@@ -18,11 +18,10 @@ document.addEventListener("DOMContentLoaded",()=> {
 })
 function setpox(base,param) {
 	const p = `<div class=cc>
- <label><input type=checkbox class=autorot>Rotate</label>
  <label><input type=checkbox class=isStereo>Stereo</label>
  <span class=oswitch><label><input type=checkbox class=pause><span class=sbtn></span></label></span> [<span class=fps></span>fps]
  <div class=pui></div></div>
-<div class=loading><img src="guruguru.gif"></div>
+<div class=loading></div>
 <div class=vrbtn></div>
 <div class=footer><a href="https://github.com/wakufactory/pox" target="_blank">PoExE</a></div>`
 	const div = document.createElement("div")
@@ -34,7 +33,8 @@ function setpox(base,param) {
 		autorot:base.querySelector(".autorot"),
 		pause:base.querySelector(".pause"),
 		isStereo:base.querySelector(".isStereo"),
-		fps:base.querySelector(".fps")}}) ;
+		fps:base.querySelector(".fps")},
+		noWebGL2:navigator.userAgent.match(/iPhone|iPad/)}) ;
 	base.poxp = poxp
 	var ss ;
 	if(param.mode>0) poxp.param.isStereo=true 
@@ -42,7 +42,7 @@ function setpox(base,param) {
 	poxp.load(param.src).then(function(src) {
 		ss = src ;
 		poxp.set(src,{param:param.param}).then(function(pox){
-			if(pox.setting.copyright) $("footer").innerHTML += "&nbsp;&nbsp;"+pox.setting.copyright;
+			if(pox.setting.copyright) base.querySelector(".footer").innerHTML += "&nbsp;&nbsp;"+pox.setting.copyright;
 			poxp.setParam(base.querySelector('.pui'))
 		})
 	}).catch(function(err) {
@@ -63,9 +63,26 @@ function setpox(base,param) {
 			if(to!=o && to.poxp) to.poxp.param.pause = true ;
 		})
 		o.poxp.param.pause = false 
-		if(!poxp.enterVR()) {
-			location.href="view.html?"+param.src
+		if(poxp.vrDisplay) poxp.enterVR() 
+		else {
+			base.style.width = "100vw" ;
+			base.style.height = "100vh" ;
+			base.style.top = "0" ; base.style.left = "0" ;
+			base.style.margin = "0" ;
+			base.style.position = "absolute" ;
+			base.querySelector(".vrbtn").style.display = "none" ;	
+			poxp.resize() ;
+			window.scrollTo(0,0);
+			poxp.pox.setting.gyro = true ;
+			window.addEventListener("deviceorientation", function(ev) {
+				var or = window.orientation ;
+				var d = (or==90||or==-90) ;
+				poxp.param.isStereo = d ;
+				base.querySelector(".cc").style.opacity = d?0.:0.4 ;
+				base.querySelector(".footer").style.display = d?"none":"block" ;
+			})
 		}
-	}) 
+	})
+
 }
 })()
